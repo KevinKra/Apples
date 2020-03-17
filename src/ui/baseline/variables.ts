3b2825a273269f0b1d16682869bd668b94bcac7a
KevinKra <kratok08@gmail.com>
2020-03-17 06:53:40 -0400

    Create styles variables resource

---
+++ src/ui/baseline/variables.ts
@@ -0,0 +1,42 @@
+const color = {
+  honeyLight: 'rgb(238,199,65)',
+  honeyDark: 'rgb(222, 168, 20)',
+
+  orangeHoneyDark: 'rgb(237,165,58)',
+  orangeHoneyLight: 'rgb(240, 223, 216)',
+  saltwaterBlue: 'rgb(61,206,207)',
+
+  snowWhite: 'rgb(255,255,255)',
+  blackInk: 'rgb(26, 26, 26)',
+  nightDark: 'rgb(0,0,0)'
+};
+
+const typography = {
+  fontFamily: {
+    openSans: '"Open Sans", "sans-serif"'
+  },
+  fontWeight: {
+    normal: '300',
+    bold: '700'
+  },
+  fontSize: {
+    veryLarge: '32px',
+    large: '20px',
+    normal: '15px',
+    small: '12px'
+  }
+};
+
+const space = {
+  page: {
+    horizontalCompact: '5rem'
+  }
+};
+
+const styles = {
+  color,
+  typography,
+  space
+};
+
+export default styles;
