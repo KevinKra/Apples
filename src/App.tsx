9a9e4262feb50e71fc77c80ef1e28823054014bb
KevinKra <kratok08@gmail.com>
2020-03-17 06:54:36 -0400

    Import OpenSans from google, setup intial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import appPage from './components/pages/app/appPage';
+import overviewPage from './components/pages/overview/overviewPage';
+import coursesPage from './components/pages/courses/coursesPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={appPage} />
+      <Route
+        exact
+        path="/courses/:course_id/overview"
+        component={overviewPage}
+      />
+      <Route exact path="/courses" component={coursesPage} />
+    </Router>
   );
 }
 
